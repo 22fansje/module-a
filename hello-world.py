1e5e195540135e3610148091f44bea981ef5437c
Jered Fanska <49483182+22fansje@users.noreply.github.com>
2019-04-10 12:46:28 -0400

    adding hello world

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+# Jered Fanska
+#04/10/2019
+#####################
+#    hello world    #
+#                   #
+#  Our First Python #
+#     Application   #
+#####################
+
+# print "Hello, World" to the screen
+print("Hello, World")
