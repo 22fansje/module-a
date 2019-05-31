a1e76bd6a9cc14a2ecc6459906459207afa5bbf7
Jered Fanska <49483182+22fansje@users.noreply.github.com>
2019-05-31 12:37:11 -0400

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    print("Hello, Python")
+##############################
+for z in range(10): 
+    print(z)
+##############################
+for y in range(1,11):
+    print(y)
+##############################
+for v in range(2,21,2):
+    print(v)
+##############################
+for x in range(1,11):
+    print(x**2)
